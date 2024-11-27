165dcfa89c19d78c22e4589581eb4dfebbca5886
Jacobamobin <jacobamobin@gmail.com>
2024-11-27 17:53:49 -0500

    Initial Commit

---
+++ DueBy/DueByApp.swift
@@ -0,0 +1,17 @@
+//
+//  DueByApp.swift
+//  DueBy
+//
+//  Created by Jacob Mobin on 2024-11-27.
+//
+
+import SwiftUI
+
+@main
+struct DueByApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
