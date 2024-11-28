c5fefdda00f15df57251b1077fdf80ee95b1ff80
Jacob Mobin <jacobamobin@gmail.com>
2024-11-27 21:25:44 -0500

    Finished item

--- DueBy/AssightmentTab/Item.swift
+++ DueBy/AssightmentTab/Item.swift
@@ -36,19 +36,42 @@ struct Item: View {
                     .frame(width: 5, height: 70)
                     .foregroundColor(eventColor)
                 
-                VStack(alignment: .leading) {
+                VStack(alignment: .leading, spacing: 2) {
                     Text(eventTitle)
                         .font(.title)
                         .fontWeight(.bold)
                         .foregroundColor(.white)
                     
+                    HStack(spacing: 4) {
+                        Image(systemName: TypeText(type).0)
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white.opacity(0.6))
 
+                        Text(TypeText(type).1)
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white.opacity(0.6))
+                    }
                     
-                    Text(TypeText(type))
                 }
                 
                 
                 Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("In 3 Days")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    
+                    Text(eventDate, format: .dateTime.day().month().year())
+                        .font(.caption)
+                        .foregroundColor(.white.opacity(0.6))
+                    
+                    Text(className)
+                    
+                }.padding(.trailing, 5)
+                
                     
             }.padding()
         }
@@ -69,36 +92,34 @@ struct Item: View {
 //9) Midterm
 //10) Exam
 
-
-
-func TypeText(_ type: Int) -> String {
+func TypeText(_ type: Int) -> (String, String) {
     switch type {
-        case 1:
-            return "Assignment"
-        case 2:
-            return "Homework"
-        case 3:
-            return "Culminating"
-        case 4:
-            return "Essay"
-        case 5:
-            return "Project"
-        case 6:
-            return "Presentation"
-        case 7:
-            return "Quiz"
-        case 8:
-            return "Test"
-        case 9:
-            return "Midterm"
-        case 10:
-            return "Exam"
-        default:
-            return "Unknown"
+    case 1:
+        return ("pencil.circle.fill", "Assignment")
+    case 2:
+        return ("book.fill", "Homework")
+    case 3:
+        return ("calendar.circle.fill", "Culminating")
+    case 4:
+        return ("doc.text.fill", "Essay")
+    case 5:
+        return ("folder.fill", "Project")
+    case 6:
+        return ("speaker.fill", "Presentation")
+    case 7:
+        return ("pencil.and.list.clipboard", "Quiz")
+    case 8:
+        return ("pencil.and.list.clipboard", "Test")
+    case 9:
+        return ("pencil.and.list.clipboard", "Midterm")
+    case 10:
+        return ("pencil.and.list.clipboard", "Exam")
+    default:
+        return ("questionmark.circle.fill", "Unknown")
     }
 }
 
 #Preview {
-    Item(eventTitle: "Culminating", eventDate: Date(), className: "Math 101", eventColor: .blue, type: 1)
+    Item(eventTitle: "Culminating", eventDate: Date(), className: "Math 101", eventColor: .blue, type: 10)
 
 }
